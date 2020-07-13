bab8047e2fa780ca76ed1d4a38bd42b508121f2e
Saber-Bjeoui <bjeoui.saber@gmail.com>
2020-07-13 22:11:38 +0100

    updating home page: adding some styles

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+$('.login-container').hide();
+$('.container').hide();
+$('.home-container').show()
 
-
-//$('.container').show('slow')
+/*$(document).click(function(){
+	$('.login-container').show(function(){
+		$(this).animate({
+			opacity: 1
+		})
+	});
+	$('.container').hide();
+	$('.home-container').hide()
+})*/
 
 
 
